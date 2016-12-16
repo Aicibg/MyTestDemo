3708def9a6f5d146231fc3d7147b61a92db4f2c1
e <aicibg@foxmail>
2016-12-16 18:13:01 +0800

    嵌套滑动，顶部下拉

---
+++ agency/src/main/java/com/tengda/agency/scroll/ScrollActivity.java
@@ -0,0 +1,15 @@
+package com.tengda.agency.scroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tengda.agency.R;
+
+public class ScrollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+    }
+}
--- agency/src/main/java/com/tengda/agency/viewdrag/ViewDragActivity.java
+++ agency/src/main/java/com/tengda/agency/viewdrag/ViewDragActivity.java
@@ -2,14 +2,25 @@ package com.tengda.agency.viewdrag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.tengda.agency.R;
 
 public class ViewDragActivity extends AppCompatActivity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_drag);
+        mTextView= (TextView) findViewById(R.id.tv_text);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTextView.offsetLeftAndRight(70);
+                Log.e("MyTest--->","tvLeft="+mTextView.getLeft());
+            }
+        });
     }
 }
